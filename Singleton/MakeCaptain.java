f1833db5d4662707a47a8abe1274e5e19eec84a3
kunjan patel <kunjanpatel@kunjans-mbp.home>
2019-08-11 17:39:19 -0400

    singleton pattern using private constructor

---
+++ Singleton/MakeCaptain.java
@@ -0,0 +1,35 @@
+package Singleton;
+
+import javax.sound.midi.Soundbank;
+
+class MakeCaptain {
+    private static MakeCaptain _captain;
+
+    //Make constructor private
+    private MakeCaptain(){}
+    
+    public static MakeCaptain getCaptain(){
+        //Lazy initialization
+        if(_captain == null){
+            _captain = new MakeCaptain();
+            System.out.println("New captain is selected for our team.");
+        } else {
+            System.out.println("You already have captain for your team.");
+            System.out.println("Send him for toss.");
+        }
+        return _captain;
+    }
+}
+
+class SingletonPatternMain{
+    public static void main(String[] args) {
+        System.out.println("Singleton pattern demo.");
+        System.out.println("Trying to make a captain for our team");
+        MakeCaptain c1 = MakeCaptain.getCaptain();
+        System.out.println("Trying to make another captain.");
+        MakeCaptain c2 = MakeCaptain.getCaptain();
+        if(c1 == c2){
+            System.out.println("c1 & c2 are same instance.");
+        }
+    }
+}
